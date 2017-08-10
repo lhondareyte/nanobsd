bc55d7e8ea89155ea23838a691cf3c122b2d6bab
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2017-08-10 20:13:06 +0200

    Adding local script to fetch/construct local ports.

---
+++ local/addLocalPorts.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+if [ -z ${NANO_PACKAGE_DIR} ] ; then
+	export NANO_PACKAGE_DIR="/usr/src/tools/tools/nanobsd/Pkg/"
+fi
+
+[ ! -d ../ports ] && exit 0
+
+for p in ../ports/*/pkg-descr ; do
+	port=$(dirname $p)
+	cd $port 
+	make 
+	make package
+	cd -
+	cp $port/wrk/pkg/*.txz ${NANO_PACKAGE_DIR}
+done
